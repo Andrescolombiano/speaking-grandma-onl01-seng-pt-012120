73f3afc8e74d5cbf065439430c67a148b21b0bb7
Andres Carvajal <jc1993box@hotmail.com>
2020-01-25 05:41:58 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -10,8 +10,14 @@
 # end
 
 def speak_to_grandma (string)
-  if string == 
-  
+  if string == "I LOVE YOU GRANDMA!"
+   "I LOVE YOU TOO PUMPKIN!"
+  elsif string != string.upcase
+   "HUH?! SPEAK UP, SONNY!"
+  else 
+    "NO, NOT SINCE 1938!"
+  end
+end
 
 
 
