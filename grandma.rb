a0dc79a8dc938cd7655d41417060f4cc228adc73
Andres Carvajal <jc1993box@hotmail.com>
2020-01-25 05:19:42 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,9 +4,9 @@ def speak_to_grandma (phrase)
     puts HUH?! SPEAK UP, SONNY!
   elsif phrase == phrase.uppercase
    puts NO, NOT SINCE 1938!
+  binding.pry
   else phrase == "I LOVE YOU GRANDMA!"
     puts I LOVE YOU TOO PUMPKIN!
-    binding.pry
  end
 end
     
